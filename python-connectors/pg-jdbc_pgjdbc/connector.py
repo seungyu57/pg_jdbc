d3fb0f7b7858a987bd94a368c3b3899853cb3a99
ssg <ssg>
2026-02-25 00:19:30 -0500

    Edited files

--- python-connectors/pg-jdbc_pgjdbc/connector.py
+++ python-connectors/pg-jdbc_pgjdbc/connector.py
@@ -16,8 +16,8 @@ class PgJdbcConnector(Connector):
         port = int(self.config.get("port", 5432))
         database = self.config.get("database", "dataiku")
         
-        user = self.config.get("user","dataiku")
-        password = self.config.get("password", "")
+        user = self.config.get("user")
+        password = self.config.get("password")
         
         schema = self.config.get("schema", "public")
         table = self.config.get("table")
